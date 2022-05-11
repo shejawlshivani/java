884e72e0da64d464c32b86408e1677342cc355f6
shejawlshivani <105193574+shejawlshivani@users.noreply.github.com>
2022-05-11 23:45:56 +0530

    Add files via upload

---
+++ Hands-on/forloop.java
@@ -0,0 +1,16 @@
+package corejava;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,n,k;
+		n=2;
+		
+		for (i=1;i<=10;i++){
+			 k = n * i;
+			System.out.println(n + "*"+ i + " = " + k);
+	}
+
+       }
+}
