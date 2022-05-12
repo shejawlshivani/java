26d693552465ab086d7563c4f1ec5b14daae121a
shejawlshivani <105193574+shejawlshivani@users.noreply.github.com>
2022-05-12 13:37:38 +0530

    Add files via upload

---
+++ Hands-on/implicitconversion.java
@@ -0,0 +1,19 @@
+package corejava;
+
+public class implicitconversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int i = 20;
+			
+			//automatic type conversion
+		    double a = i;
+			
+			//automatic type conversion
+			
+			System.out.println("Int value: "+i);
+			System.out.println("double value: "+a);
+			
+	}
+
+}
