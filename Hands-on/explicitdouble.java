ec90f7a84c06a652da6ae1bf3a357895cb7502ed
shejawlshivani <105193574+shejawlshivani@users.noreply.github.com>
2022-05-12 13:36:59 +0530

    Add files via upload

---
+++ Hands-on/explicitdouble.java
@@ -0,0 +1,15 @@
+package corejava;
+
+public class explicitdouble {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//convert double to integer
+		double d = 100.2;
+		
+		int i =(int)d;
+		System.out.println("Integer Data type value"+i);
+
+	}
+
+}
