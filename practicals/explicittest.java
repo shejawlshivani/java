ae5890e962475a0f5a73b11739919bb84e6874c7
shejawlshivani <105193574+shejawlshivani@users.noreply.github.com>
2022-05-12 12:27:09 +0530

    Add files via upload

---
+++ practicals/explicittest.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class explicittest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+byte b = 70;
+		
+		b = (byte)(b * 2);
+		System.out.println(b);
+	}
+
+}
