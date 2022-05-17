5d708330c522679731a42a50153a2f186f025ba2
shejawlshivani <105193574+shejawlshivani@users.noreply.github.com>
2022-05-17 11:35:10 +0530

    Add files via upload

---
+++ practicals/Singleinheritance.java
@@ -0,0 +1,16 @@
+package corejava;
+
+
+class Singleinheritance {
+public void print() {
+System.out.println("shivani");
+
+       }
+ }
+class SimpleOne extends Singleinheritance{
+	public void show() {
+		System.out.println("shejawl");
+	}
+}
+
+
