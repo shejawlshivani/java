c4d136aba0a8535ee8b2100cc1e591f9051f622c
shejawlshivani <105193574+shejawlshivani@users.noreply.github.com>
2022-05-18 13:01:49 +0530

    Add files via upload

---
+++ practicals/LogicalNot.java
@@ -0,0 +1,16 @@
+package corejava;
+
+public class LogicalNot {
+
+	public static void main(String[] args) {
+		//initializing variable
+		int a= 10, b=1;
+		 // Displaying a, b, c
+		System.out.println("Var1 = " + a); 
+		System.out.println("Var2 = " + b); 
+		// Using logical NOT operator
+		System.out.println("!(a < b) = " + !(a < b));
+		System.out.println("!(a > b) = " + !(a > b));
+	}
+
+}
