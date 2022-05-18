e384db74ab9a5e1864d830b635a88ecc0402c899
shejawlshivani <105193574+shejawlshivani@users.noreply.github.com>
2022-05-18 11:00:24 +0530

    Add files via upload

---
+++ practicals/encapsulation.java
@@ -0,0 +1,46 @@
+package corejava;
+
+public class encapsulation {
+
+	// private variables declared
+		// these can only be accessed by
+	    // public methods of class
+		
+		private String name;
+		private int rollno;
+		private int age;
+		
+		//get method for name
+		public String getName() {
+			return name;
+		}
+		
+		//set method name
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		//get method for roll no
+		public int getRollno() {
+			return rollno;
+		}
+		
+		//set method for roll no
+		public void setRollno(int rollno) {
+			this.rollno = rollno;
+		}
+		
+		//get method for age
+		public int getAge() {
+			return age;
+		}
+		
+		//set method for age
+		public void setAge(int age) {
+			this.age = age;
+		}
+	
+
+	}
+
+
