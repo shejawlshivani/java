4abccdb7d247e992849f27b59888c7bfd7e8c3dd
shejawlshivani <105193574+shejawlshivani@users.noreply.github.com>
2022-05-18 10:58:28 +0530

    Add files via upload

---
+++ practicals/abstraction.java
@@ -0,0 +1,20 @@
+package corejava;
+
+//creating abstract class
+	abstract class abstraction {
+
+		//creating variable
+		
+		public abstract int Multiply(int n1, int n2);
+		public abstract int Multiplyone(int n1, int n2, int n3);
+	
+			
+	}
+
+	
+
+	
+
+	
+
+
