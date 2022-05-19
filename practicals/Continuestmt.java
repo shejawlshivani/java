1901fa8274407e1e429289742659f90663a0ceb1
shejawlshivani <105193574+shejawlshivani@users.noreply.github.com>
2022-05-19 11:02:23 +0530

    Add files via upload

---
+++ practicals/Continuestmt.java
@@ -0,0 +1,19 @@
+package corejava;
+
+public class Continuestmt {
+
+	public static void main(String[] args) {
+		
+			for (int i=0; i<10; i++) {
+				if (i==4) {
+					continue;
+				
+				}
+			
+				
+				System.out.println(i);
+
+	}
+
+	   }	
+}
