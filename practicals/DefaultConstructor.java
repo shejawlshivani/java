816cb6fafd79153d19ca6a463dfd6d97a80b6e0d
shejawlshivani <105193574+shejawlshivani@users.noreply.github.com>
2022-05-19 12:00:56 +0530

    Add files via upload

---
+++ practicals/DefaultConstructor.java
@@ -0,0 +1,14 @@
+package corejava;
+
+ class DefaultConstructor {
+   DefaultConstructor()
+
+ {
+	System.out.println("This is default constructor/no argument constructor");
+ }
+	public static void main(String[] args) {
+		DefaultConstructor abc = new DefaultConstructor();
+
+	}
+
+}
