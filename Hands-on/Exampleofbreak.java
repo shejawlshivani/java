6e158dc3cdbc13b4f03a3db14f456b0bdb36b8e7
shejawlshivani <105193574+shejawlshivani@users.noreply.github.com>
2022-05-20 10:22:03 +0530

    Add files via upload

---
+++ Hands-on/Exampleofbreak.java
@@ -0,0 +1,16 @@
+package corejava;
+
+public class Exampleofbreak {
+
+	public static void main(String[] args) {
+		for (int i=0; i<10; i++) {
+			if (i==9) {
+				break;
+			
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
