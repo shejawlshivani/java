2c9be9a588244ee07f497a2bfe372fc8ea3aefed
shejawlshivani <105193574+shejawlshivani@users.noreply.github.com>
2022-05-20 14:10:32 +0530

    Add files via upload

---
+++ Hands-on/Onedimensionalarray.java
@@ -0,0 +1,22 @@
+package corejava;
+
+ class Onedimensionalarray {
+
+	public static void main(String[] args) {
+		//declare and initialize one dimension array
+		
+		String[] str = new String[] {"shivani","diya","dipa","jaya"};
+		System.out.println("these are elements are one dimensional array");
+		
+		for(int i=0; i<str.length; i++) {
+			System.out.println(str[i] + " ");
+		}
+
+			
+							
+				 
+			
+	
+
+}
+ }
