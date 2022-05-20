bd98b85959a383e1d2723ace15adefce921311cc
shejawlshivani <105193574+shejawlshivani@users.noreply.github.com>
2022-05-20 14:09:02 +0530

    Add files via upload

---
+++ practicals/Multidimesionalarray.java
@@ -0,0 +1,27 @@
+package corejava;
+
+ class Multidimesionalarray {
+
+	public static void main(String[] args) {
+		
+		// Declaring and initializing multidimensional array
+		int arr[][]= {{2, 7, 9},{ 5, 8, 7},{1, 3, 4}};
+		
+		//Printing array
+		for (int i=0; i<3; i++)
+		{
+		for (int j=0; j<3; j++)
+		
+			System.out.print(arr[i][j]+" ");
+			System.out.println();
+		}
+		
+			{
+				
+			
+		}
+	
+ }
+ }	
+ 	
+
