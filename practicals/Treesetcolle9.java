3c1cbbad5764b669804ece7dc6df1398965e8d9c
shejawlshivani <105193574+shejawlshivani@users.noreply.github.com>
2022-05-24 13:29:44 +0530

    Add files via upload

---
+++ practicals/Treesetcolle9.java
@@ -0,0 +1,26 @@
+package corejava;
+import java.util.*;
+public class Treesetcolle9 {
+
+	public static void main(String[] args) {
+		
+		//Creating and adding elements  
+		TreeSet<String> set=new TreeSet<String>();  
+		set.add("Ravi");  
+		set.add("Vijay");  
+		set.add("Ravi");  
+		set.add("Ajay");  
+		
+		//traversing elements  
+		Iterator<String> itr=set.iterator();  
+		while(itr.hasNext()){  
+		System.out.println(itr.next());  
+		}  
+		}  
+		
+
+
+
+	}
+
+
