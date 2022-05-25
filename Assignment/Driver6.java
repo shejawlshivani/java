f8aeaeba2de60381a615bada2b4b643a02e4e63d
shejawlshivani <105193574+shejawlshivani@users.noreply.github.com>
2022-05-25 11:55:41 +0530

    Add files via upload

---
+++ Assignment/Driver6.java
@@ -0,0 +1,26 @@
+package corejava;
+
+public class Driver6 {
+
+	public static void main(String[] args) {
+		int [] intArr = new int [5]; 
+		int arr[] = { 11, 22, 33 };
+		for (int i = 0; i < arr.length; i++)
+		System.out.print(arr[i] + " ");
+		System.out.println();
+		
+		int arr2[] = new int[3];
+	
+		int arr3[] = { 11, 22, 33 };
+
+		for (int i = 0; i < arr3.length; i++)
+		System.out.print(arr3[i] + " ");
+		}
+
+
+		
+
+
+	}
+	
+
