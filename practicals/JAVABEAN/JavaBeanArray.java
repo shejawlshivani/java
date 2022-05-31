f2a40c2df3b6545d0090d1c6af51f7de141489cb
shejawlshivani <105193574+shejawlshivani@users.noreply.github.com>
2022-05-31 12:21:09 +0530

    Add files via upload

---
+++ practicals/JAVABEAN/JavaBeanArray.java
@@ -0,0 +1,56 @@
+package corejava;
+
+public class JavaBeanArray {
+
+	public int id;
+	public String name;
+	public String[] Subjects;
+
+		public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String[] getSubjects() {
+		return Subjects;
+	}
+
+	public void setSubjects( String[]subjects) {
+		this.Subjects=subjects;
+		
+	}
+
+		public static void main(String[] args) {
+	JavaBeanArray student=new JavaBeanArray();
+	String[] Subject= {"ENGLISH","SCIENCE","COMPUTER"};
+	 
+	//setting bean values
+	 student.setId(101);
+	student.setName("Shivani");
+	student.setSubjects(Subject);
+
+	//getting bean values
+	System.out.println(" Student id:"+student.getId());
+	System.out.println(" Student name:"+student.getName());
+
+
+	String[] subjectArray = student.getSubjects();
+	for (int i = 0; i < subjectArray.length; i++) {
+		System.out.println("Student subject " + (i + 1) + " : " + subjectArray[i]);
+
+		}
+
+	}
+
+}
---
+++ practicals/JAVABEAN/JavaBeanEmployee.java
@@ -0,0 +1,41 @@
+package corejava;
+
+
+	import java.io.Serializable;
+
+	public class JavaBeanEmployee implements Serializable {
+
+		public JavaBeanEmployee() {
+		}
+		private int id;
+	   private String name;
+
+		public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+		public static void main(String[] args) {
+
+			JavaBeanEmployee e=new JavaBeanEmployee();
+			e.setId(5);
+			e.setName("Shreya");
+			System.out.println("id:"+e.getId());
+			System.out.println("name:"+e.getName());
+			}
+
+	
+	}
+
+
