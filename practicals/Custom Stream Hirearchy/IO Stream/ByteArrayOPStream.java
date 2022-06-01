a0e8d379ce431852b40c5e335843601f08861b6d
shejawlshivani <105193574+shejawlshivani@users.noreply.github.com>
2022-06-01 14:20:01 +0530

    Add files via upload

---
+++ practicals/Custom Stream Hirearchy/IO Stream/ByteArrayOPStream.java
@@ -0,0 +1,31 @@
+package corejava;
+
+import java.io.*;
+
+
+ class ByteArrayOPStream {
+
+	public static void main(String[] args) throws Exception {
+
+		//Creating FileOutputStream object
+		FileOutputStream fout1=new FileOutputStream("D:\\MyFile"); 
+		
+	      FileOutputStream fout2=new FileOutputStream("D:\\CopyofMyFile");    
+	        
+	      ByteArrayOutputStream bout=new ByteArrayOutputStream();    
+	      bout.write(65);    
+	      bout.writeTo(fout1);    
+	      bout.writeTo(fout2);    
+	        
+	      bout.flush();    
+	      bout.close();//has no effect    
+	      System.out.println("Success...");    
+	     }    
+	    
+
+
+		
+
+	}
+
+
