374fb974c407f6836fa79e99b86ea147769ba534
shejawlshivani <105193574+shejawlshivani@users.noreply.github.com>
2022-06-01 15:35:46 +0530

    Add files via upload

---
+++ practicals/Custom Stream Hirearchy/IO Stream/FilterOPStrm.java
@@ -0,0 +1,24 @@
+package corejava;
+import java.io.*;
+public class FilterOPStrm {
+
+	public static void main(String[] args) throws IOException{
+        File data = new File("D:\\testout.txt");  
+        FileOutputStream file = new FileOutputStream(data);  
+        FilterOutputStream filter = new FilterOutputStream(file);  
+        String s="Welcome to Mumbai.";      
+        byte b[]=s.getBytes();      
+        filter.write(b);     
+        filter.flush();  
+        filter.close();  
+        file.close();  
+        System.out.println("Success...");  
+    }  
+
+
+
+		
+
+	}
+
+
